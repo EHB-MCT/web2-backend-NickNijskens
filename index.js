ad9083e5723cd87c6607ba80e66efe64c95c56cd
NickNijskens <nick.nijskens@student.ehb.be>
2021-12-14 11:39:52 +0100

    bugfix index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
-require('dotenv').config();
+require('dotenv').config()
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
